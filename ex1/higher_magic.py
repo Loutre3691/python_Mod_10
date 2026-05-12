e40fca91eed087db7712a08f3f285b48821507fe
Flora De Checchi <flora.dechecchi@gmail.com>
2026-05-12 18:05:13 +0200

    ex1 debut

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -1,25 +1,48 @@
 from typing import Callable
 
-def spell(target: str, power: int) -> str:
-    return (f"spell of target {target} ang power {power}")
+
+class Spells:
+    
+    def fireball(self, target):
+        return f"🔥 Fireball hits {target},"
+    
+    def heals(self, target):
+        return f"🏥 Heals hits {target}"
+
 
 def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
-    combiner = callable()
-    return combiner
+    print("\n\033[1;34mTesting spell combiner...\033[0m]")
+    if not callable(spell1) or not callable(spell2):
+        raise TypeError("Both spells must be functions!!")
 
+    def combined(*args) -> str:
+        res1 = spell1(*args)
+        res2 = spell2(*args)
+        return f"{res1} {res2}"
 
-def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
-    amplifer = callable()
-    return amplifer
-
-
-def conditional_caster(condition: Callable, spell: Callable) -> Callable:
-    caster = callable()
-    return caster
-
-
-def spell_sequence(spells: list[Callable]) -> Callable:
-    sequence = callable()
+    return combined
 
 if __name__ == "__main__":
-    print("ok")
+    s = Spells()
+
+    combined = spell_combiner(s.fireball, s.heals)
+    print(f"Combined spell result: {combined('Dragon')}")
+
+# def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
+#     print("\n\033[1;34mTesting power amplifer...\033[0m]")
+#     amplifer = callable()
+#     return amplifer
+# Vérifier que base_spell est une fonction
+
+
+# def conditional_caster(condition: Callable, spell: Callable) -> Callable:
+#     print("\n\033[1;34mTesting conditional caster...\033[0m]")
+#     caster = callable()
+#     return caster
+# Vérifier que condition et spell sont des fonctions
+
+# def spell_sequence(spells: list[Callable]) -> Callable:
+#     print("\n\033[1;34mTesting spell sequence...\033[0m]")
+#     sequence = callable()
+#     return sequence
+# Vérifier que chaque élément de la liste est une fonction
