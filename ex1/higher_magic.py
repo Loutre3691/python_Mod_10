6063586e5e7f086d49e18e19aed07092740ed118
Flora De Checchi <flora.dechecchi@gmail.com>
2026-05-14 02:24:27 +0200

    ex1 milieu

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -3,39 +3,58 @@ from typing import Callable
 
 class Spells:
     
-    def fireball(self, target):
-        return f"🔥 Fireball hits {target},"
+    def fireball(self, target, power=5):
+        return f"🔥 Fireball hits {target}"
     
-    def heals(self, target):
+    def heals(self, target, power=10):
         return f"🏥 Heals hits {target}"
 
 
 def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
-    print("\n\033[1;34mTesting spell combiner...\033[0m]")
+    
     if not callable(spell1) or not callable(spell2):
         raise TypeError("Both spells must be functions!!")
 
-    def combined(*args) -> str:
-        res1 = spell1(*args)
-        res2 = spell2(*args)
-        return f"{res1} {res2}"
+    def combined(target, power=5) -> str:  # ← les vrais arguments
+        res1 = spell1(target, power)        # ← on APPELLE spell1
+        res2 = spell2(target, power)        # ← on APPELLE spell2
+        return f"{res1} , {res2}"
 
     return combined
-
 # spell_combiner check that spell1 and spell2 it's a function with callable
 # return tupple
 
-if __name__ == "__main__":
-    s = Spells()
 
-    combined = spell_combiner(s.fireball, s.heals)
-    print(f"Combined spell result: {combined('Dragon')}")
+def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
+    print("\n\033[1;34mTesting power amplifer...\033[0m")
+    if not callable(base_spell):
+        raise TypeError("Base spell must be function")
+    
+    def amplifer(value) -> int:
+        before_amplif = value
+        after_amplif = before_amplif * multiplier
+        return before_amplif, after_amplif
+    
 
-# def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
-#     print("\n\033[1;34mTesting power amplifer...\033[0m]")
-#     amplifer = callable()
-#     return amplifer
+    return amplifer
+
 # Vérifier que base_spell est une fonction
+
+
+if __name__ == "__main__":
+        
+    spells = Spells()
+    combo = spell_combiner(spells.fireball, spells.heals)
+    print(combo("Dragon"))
+
+
+    power = power_amplifier(spells.fireball, 3)
+    print(spells.fireball('dragon'))
+    print(power(10))
+    heal = power_amplifier(spells.heals, 3)
+    print(heal(5))
+
+
 
 
 # def conditional_caster(condition: Callable, spell: Callable) -> Callable:
